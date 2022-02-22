a20eac6bff8f58e2254ec90b5d98c5f0cbd415e0
AlexandrShyshkov <89694740+AlexandrShyshkov@users.noreply.github.com>
2022-02-22 23:32:20 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,299 @@
+"use strict"
+// BURGER
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.header__nav');
+const navItems = nav.querySelectorAll('a');
+const body = document.body;
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
+
+burger.addEventListener('click', () => {
+    body.classList.toggle('no-scroll');
+    burger.classList.toggle('active');
+    nav.classList.toggle('header__visible');
+});
+
+navItems.forEach(el => {
+    el.addEventListener('click', () => {
+        body.classList.remove('no-scroll');
+        burger.classList.remove('active');
+        nav.classList.remove('header__visible');
+    });
+});
+// TAB
+document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        const id = e.target.getAttribute('href').replace('#', '');
+
+        document.querySelectorAll('.tabs-triggers__item').forEach(
+            (child) => child.classList.remove('tabs-triggers__item--active')
+        );
+        document.querySelectorAll('.tabs-content__item').forEach(
+            (child) => child.classList.remove('tabs-content__item--active')
+        );
+
+        item.classList.add('tabs-triggers__item--active');
+        document.getElementById(id).classList.add('tabs-content__item--active');
+
+    })
+);
+document.querySelector('.tabs-triggers__item').click();
+// TIMER
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector(".days");
+    var hoursSpan = clock.querySelector(".hours");
+    var minutesSpan = clock.querySelector(".minutes");
+    var secondsSpan = clock.querySelector(".seconds");
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        if (t.total <= 0) {
+            document.getElementById("countdown").className = "hidden";
+            document.getElementById("deadline-message").className = "visible";
+            clearInterval(timeinterval);
+            return true;
+        }
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+        minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+var deadline = '2022-08-01';
+// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('countdown', deadline);
+initializeClock('countdown-2', deadline);
+initializeClock('countdown-3', deadline);
+initializeClock('countdown-4', deadline);
+initializeClock('countdown-5', deadline);
+initializeClock('countdown-6', deadline);
+initializeClock('countdown-7', deadline);
+initializeClock('countdown-8', deadline);
+
+// SPOLLER
+const spollersArray = document.querySelectorAll('[data-spollers]');
+if (spollersArray.length > 0) {
+    // Получение обычных слайдеров
+    const spollersRegular = Array.from(spollersArray).filter(function(item, index, self) {
+        return !item.dataset.spollers.split(",")[0];
+    });
+    // Иницилизация обычных спойлеров
+    if (spollersRegular.length > 0) {
+        initSpollers(spollersRegular);
+    }
+
+    // Получение слайдеров с медиа запросами
+    const spollersMedia = Array.from(spollersArray).filter(function(item, index, self) {
+        return item.dataset.spollers.split(",")[0];
+    });
+
+    // Иницилизация спойлеров с медиа запросами
+    if (spollersMedia.length > 0) {
+        const breakpointsArray = [];
+        spollersMedia.forEach(item => {
+            const params = item.dataset.spollers;
+            const breakpoint = {};
+            const paramsArray = params.split(",");
+            breakpoint.value = paramsArray[0];
+            breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : "max";
+            breakpoint.item = item;
+            breakpointsArray.push(breakpoint);
+        });
+
+        //Получение уникальных брейкпоинтов
+        let mediaQueries = breakpointsArray.map(function(item) {
+            return '(' + item.type + "-width: " + item.value + "px)," + item.value + ',' + item.type;
+        });
+        mediaQueries = mediaQueries.filter(function(item, index, self) {
+            return self.indexOf(item) === index;
+        });
+
+        // Работаем с каждым брейкпоинтом
+        mediaQueries.forEach(breakpoint => {
+            const paramsArray = breakpoint.split(",");
+            const mediaBreakpoint = paramsArray[1];
+            const mediaType = paramsArray[2];
+            const matchMedia = window.matchMedia(paramsArray[0]);
+
+            // Объекты с нужными условиями
+            const spollersArray = breakpointsArray.filter(function(item) {
+                if (item.value === mediaBreakpoint && item.type === mediaType) {
+                    return true;
+                }
+            });
+            // Событие
+            matchMedia.addListener(function() {
+                initSpollers(spollersArray, matchMedia);
+            });
+            initSpollers(spollersArray, matchMedia);
+        });
+    }
+    // Иницилизация
+    function initSpollers(spollersArray, matchMedia = false) {
+        spollersArray.forEach(spollersBlock => {
+            spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
+            if (matchMedia.matches || !matchMedia) {
+                spollersBlock.classList.add('_init');
+                initSpollerBody(spollersBlock);
+                spollersBlock.addEventListener("click", setSpollerAction);
+            } else {
+                spollersBlock.classList.remove('_init');
+                initSpollerBody(spollersBlock, false);
+                spollersBlock.removeEventListener("click", setSpollerAction);
+            }
+        });
+    }
+    // Работа с контентом
+    function initSpollerBody(spollersBlock, hideSpollerBody = true) {
+        const spollerTitles = spollersBlock.querySelectorAll('[data-spoller');
+        if (spollerTitles.length > 0) {
+            spollerTitles.forEach(spollerTitle => {
+                if (hideSpollerBody) {
+                    spollerTitle.removeAttribute('tabindex');
+                    if (!spollerTitle.classList.contains('_active')) {
+                        spollerTitle.nextElementSibling.hidden = true;
+                    }
+                } else {
+                    spollerTitle.setAttribute('tabindex', '-1');
+                    spollerTitle.nextElementSibling.hidden = false;
+                }
+            });
+        }
+    }
+
+    function setSpollerAction(e) {
+        const el = e.target;
+        if (el.hasAttribute('data-spoller') || el.closest('[data-spoller]')) {
+            const spollerTitle = el.hasAttribute('data-spoller') ? el : el.closest('[data-spoller]');
+            const spollersBlock = spollerTitle.closest('[data-spollers]');
+            const oneSpoller = spollersBlock.hasAttribute('data-one-spoller') ? true : false;
+            if (!spollersBlock.querySelectorAll('._slide').length) {
+                if (oneSpoller && !spollerTitle.classList.contains('_active')) {
+                    hideSpollersBody(spollersBlock);
+                }
+                spollerTitle.classList.toggle('_active');
+                _slideToggle(spollerTitle.nextElementSibling, 500);
+            }
+            e.preventDefault();
+        }
+    }
+
+    function hideSpollersBody(spollersBlock) {
+        const spollerActiveTitle = spollersBlock.querySelector('[data-spoller]._active');
+        if (spollerActiveTitle) {
+            spollerActiveTitle.classList.remove('_active');
+            _slideUp(spollerActiveTitle.nextElementSibling, 500);
+        }
+    }
+}
+
+// ===============================
+// SlideToggle
+let _slideUp = (target, duration = 500) => {
+    if (!target.classList.contains('_slide')) {
+        target.classList.add('_slide');
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = target.offsetHeight + 'px';
+        target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        window.setTimeout(() => {
+            target.hidden = true;
+            target.style.removeProperty('height');
+            target.style.removeProperty('padding-top');
+            target.style.removeProperty('padding-bottom');
+            target.style.removeProperty('margin-top');
+            target.style.removeProperty('margin-bottom');
+            target.style.removeProperty('overflow');
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+            target.classList.remove('_slide');
+        }, duration);
+    }
+}
+let _slideDown = (target, duration = 500) => {
+    if (!target.classList.contains('_slide')) {
+        target.classList.add('_slide');
+        if (target.hidden) {
+            target.hidden = false;
+        }
+        let height = target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        target.offsetHeight;
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height + 'px';
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        window.setTimeout(() => {
+            target.style.removeProperty('height');
+            target.style.removeProperty('overflow');
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+            target.classList.remove('_slide');
+        }, duration);
+    }
+}
+let _slideToggle = (target, duration = 500) => {
+    if (target.hidden) {
+        return _slideDown(target, duration);
+    } else {
+        return _slideUp(target, duration);
+    }
+}
+
+/*
+Для родителя спойлеров пишем атрибут data-spollers
+Для заголовка спойлеров пишем атрибут data-spoller
+Если нужно вкл/выкл работу спойлеров на разных размерах экранов
+пишем параметры шины и тип брейкпоинта
+Пример:
+data-spollers="992,max" - спойлеры будут работать только на экранах меньне или равно 992px
+data-spollers="768",min" - спойлеры будут работать только на экранах больше или равно 768px
+Если нужно чтобы в блоке открывался только один спойлер добавляем атрибут data-one-spoller
+*/
+// SLIDER
+
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+    });
+});
